4fe9cbcdbbd3e54f9d8a3fd4fa69899b96f64c4b
Dominik-Wojcik <domek99@gmail.com>
2019-12-17 10:33:25 +0100

    added joking

--- gittie.py
+++ gittie.py
@@ -1,2 +1,11 @@
 def introduce():
     print("Hello, i'm Gittie!")
+
+def joke():
+    print(
+        "5. Knock knock. \n"
+        "Who’s there? \n"
+        "Robin. \n"
+        "Robin who? \n"
+        "Robin you, now hand over the cash. \n"
+    )
